9627acaa5898e887ef8aeb0e7fe6d5e3d11fdada
Dmytro Chyr <dima543439@gmail.com>
2023-10-16 01:15:54 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { FloatButton } from 'antd';
+import "./App.scss";
+import MainPage from "./components/MainPage/Main";
+import DownloadOutlined from "@ant-design/icons/lib/icons/DownloadOutlined";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <FloatButton icon= {<DownloadOutlined />} onClick={() => console.log("click")} />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
