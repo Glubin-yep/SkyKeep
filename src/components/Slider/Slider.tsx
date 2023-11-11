57065b98e364a03ba3cb4dfdeffa830a0d131b79
Dmytro Chyr <dima543439@gmail.com>
2023-11-12 00:40:22 +0200

    Replacing the logo, renaming buttons

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,52 +1,55 @@
-import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  CloudOutlined,
+  DesktopOutlined,
+  HistoryOutlined,
+} from "@ant-design/icons";
 import { Menu, Layout, MenuProps } from "antd";
 import React, { useState } from "react";
-import logo from "../../assets/react.svg";
+import logo from "../../assets/logo.svg";
+import "../Slider/Slider.css";
 
 const { Sider } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    } as MenuItem;
-  }
-  
-export default function Slider() {
-    
-    const [collapsed, setCollapsed] = useState(false);
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
 
-    const items: MenuItem[] = [
-        getItem('Option 1', '1', <PieChartOutlined />),
-        getItem('Option 2', '2', <DesktopOutlined />),
-        getItem('User', 'sub1', <UserOutlined />, [
-          getItem('Tom', '3'),
-          getItem('Bill', '4'),
-          getItem('Alex', '5'),
-        ]),
-        getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-        getItem('Files', '9', <FileOutlined />),
-      ];
-  
+export default function Slider() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const items: MenuItem[] = [
+    getItem("Storage", "1", <CloudOutlined />),
+    getItem("Activity", "2", <DesktopOutlined />),
+    getItem("History", "3", <HistoryOutlined />),
+  ];
+
   return (
     <Sider
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
     >
-      <div className="demo-logo-vertical">
-        <img src={logo} alt=" " />
+      <div className="logo-vertical">
+        <img className="logo-image" src={logo} alt=" " />
+        <span className="logo-text" hidden={collapsed}>
+          Sky Keep
+        </span>
       </div>
+
       <Menu
+        className="Menu"
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
