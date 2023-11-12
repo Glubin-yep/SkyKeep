6e66ea74cddf2a41423f5481ad7e2521d261aa5f
Dmytro Chyr <dima543439@gmail.com>
2023-11-13 01:22:32 +0200

    Add nav routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import { FloatButton } from 'antd';
 import "./App.css";
 import MainPage from "./components/MainPage/Main";
 import DownloadOutlined from "@ant-design/icons/lib/icons/DownloadOutlined";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <FloatButton icon= {<DownloadOutlined />} onClick={() => console.log("click")} />
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,28 +1,23 @@
-import { Breadcrumb, Layout, theme } from "antd";
-import React from "react";
+import { Layout } from "antd";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
+import Activity from "./Activity/Activity";
+import History from "./History/History";
+import './Content.css'
 
 const { Content } = Layout;
 
 function ContentLayout() {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
+ 
   return (
-    <Content style={{ margin: "0 16px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>User</Breadcrumb.Item>
-        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-      </Breadcrumb>
-      <div
-        style={{
-          padding: 24,
-          minHeight: 360,
-          background: colorBgContainer,
-        }}
-      >
-        Bill is a cat.
-      </div>
+    <Content className="content">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/activity" element={<Activity />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </Router>
     </Content>
   );
 }
