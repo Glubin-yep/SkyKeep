8b0e1da9b59b1e2824fbb11674dde15e041901d7
Dmytro Chyr <dima543439@gmail.com>
2023-12-08 02:23:51 +0200

    Add component for user history

---
+++ src/Types/UserHistory.type.ts
@@ -0,0 +1,7 @@
+export type UserHistoryType = {
+  id: number;
+  fileId: number;
+  fileName: string;
+  action: string;
+  createdOn: Date;
+};
--- src/components/Content/History/History.tsx
+++ src/components/Content/History/History.tsx
@@ -1,3 +1,53 @@
-export default function History() {
-  return <div>History</div>;
-}
+import { useState, useEffect } from 'react';
+import { Table } from 'antd';
+import api from '../../../http';
+import moment from 'moment';
+import { UserHistoryType } from '../../../Types/UserHistory.type';
+
+const History = () => {
+  const [historyData, setHistoryData] = useState([]);
+
+  useEffect(() => {
+    api.get("files/getHistory")
+      .then(response => {
+        const formattedData = response.data.map((item: UserHistoryType) => ({
+          ...item,
+          createdOn: moment(item.createdOn).format('YYYY-MM-DD HH:mm:ss'),
+          key: item.id,
+        }));
+        setHistoryData(formattedData);
+      })
+      .catch(error => console.error('Error fetching activity data:', error));
+  }, []);
+  
+  const columns = [
+    {
+      title: 'Action Time',
+      dataIndex: 'createdOn',
+      key: 'createdOn',
+    },
+    {
+      title: 'File ID',
+      dataIndex: 'fileId',
+      key: 'fileId',
+    },
+    {
+      title: 'File Name',
+      dataIndex: 'fileName',
+      key: 'fileName',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+    },
+  ];
+
+  return (
+    <div>
+      <Table dataSource={historyData} columns={columns} rowKey="id" />
+    </div>
+  );
+};
+
+export default History;
