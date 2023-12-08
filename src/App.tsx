f6a5d4bc409bb6c1f8efcca2e3f11200b934f95f
Dmytro Chyr <dima543439@gmail.com>
2023-12-08 02:25:09 +0200

    Add Error 404 handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MainPage from "./components/MainPage/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login/Login";
 import Registration from "./components/Auth/Registration/Registration";
+import Error404 from "./components/ErrorPage/Error404";
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
             <Route path="/" element={<MainPage />} />
             <Route path="/auth/registration" element={<Registration />} />
             <Route path="/auth/login" element={<Login />} />
+            <Route path="*" element={<Error404 />} />
           </Routes>
         </BrowserRouter>
       </ConfigProvider>
--- src/components/ErrorPage/Error404.tsx
+++ src/components/ErrorPage/Error404.tsx
@@ -1,3 +1,21 @@
-export default function Error404() {
-  return <div>Error404</div>;
-}
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+import "./Error404.css";
+
+const Error404 = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">404 - Not Found</h1>
+      <p className="textStyle">
+        Oops! The page you are looking for might be in another castle.
+      </p>
+      <Link to="/">
+        <Button type="primary" size="large">
+          Go Home
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Error404;
