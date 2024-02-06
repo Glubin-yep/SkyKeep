d0aa0e38c569c8d048dc466a5dce8bd88ee3c3e5
Dmytro Chyr <dima543439@gmail.com>
2024-02-06 10:21:28 +0200

    Clean up AuthService

--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,18 +1,10 @@
 import axios from "axios";
-import AuthService from "../service/AuthService";
-
-const AuthToken = AuthService.getAuthToken();
 
 export const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${AuthToken}`;
-  return config;
 });
 
 export default api;
--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,5 +1,5 @@
 import { UserType } from "../Types/User.type";
-import api from "../http";
+import api, { API_URL } from "../http";
 import Cookies from "universal-cookie";
 
 export default class AuthService {
@@ -51,67 +51,22 @@ export default class AuthService {
     return {} as UserType;
   }
 
-  static getAuthToken(): string | null {
-    const cookies = new Cookies();
-    const cookie = cookies.get("Authorization");
-    if (cookie) {
-      return cookie.token;
-    } else {
-      const userStr = localStorage.getItem("user");
-      if (userStr) {
-        return JSON.parse(userStr).token;
-      }
-    }
-    return null;
-  }
-
   static async isValidToken(): Promise<boolean> {
-    const cookies = new Cookies();
-    const cookie = cookies.get("Authorization");
-    if (cookie) {
-      try {
-        const response = await api.get(`/auth/validate/${cookie.token}`);
+    try {
+      const response = await api.get(`/auth/validate`);
 
-        if (response.data.statusCode === 401) {
-          return false;
-        }
-        return true;
-      } catch (error) {
+      if (response.data.statusCode === 401) {
         return false;
       }
-    } else {
-      const userStr = localStorage.getItem("user");
-
-      if (userStr) {
-        const token = JSON.parse(userStr).token;
-
-        try {
-          const response = await api.get(`/auth/validate/${token}`);
-
-          if (response.data.statusCode === 401) {
-            return false;
-          }
-          return true;
-        } catch (error) {
-          return false;
-        }
-      }
+      return true;
+    } catch (error) {
+      return false;
     }
-
-    return false;
   }
 
   static async GithubLogin(): Promise<any> {
-    //   return api.get("auth/github").then((response) => {
-    //     if (response.data.token) {
-    //       localStorage.setItem("user", JSON.stringify(response.data));
-    //     }
-    //     return response.data;
-    //   });
-
     try {
-      window.location.href =
-        "https://discrete-akita-quality.ngrok-free.app/auth/github";
+      window.location.href = API_URL + "auth/github";
     } catch (ex) {
       console.log(ex);
     }
